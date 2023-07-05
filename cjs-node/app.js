858e471bbf7254e189dae38fecab07b459a3ab68
gaowujian <gaowujian@bytedance.com>
2023-07-05 10:42:27 +0800

    feat: update3

--- cjs-node/app.js
+++ cjs-node/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const path = require("path");
 const userList = require("./user");
 const app = express();
 
+// app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// 后端模板引擎渲染页面
 app.get("/", (req, res) => {
-  res.send("welcome");
+  res.render("index", {
+    content: "初始空白内容",
+    users: [],
+  });
 });
+
+// 后端直接渲染内容
 app.get("/user", (req, res) => {
+  res.render("index", {
+    content: "用户列表",
+    users: [11, 22, 33],
+  });
+});
+
+// 接口api服务
+app.get("/users", (req, res) => {
   res.send(userList);
 });
 
